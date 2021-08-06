9eec98cc425b77649d02bd3f34a9f4db265b7e25
TheJeme <eemelijoonatan@gmail.com>
2021-08-06 19:01:59 +0300

    part7 country-hook

---
+++ part_7/country-hook/src/hooks/index.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    if (name.length > 0) {
+      axios
+        .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+        .then((response) => {
+          setCountry({ data: response.data[0], found: true });
+        })
+        .catch(() => {
+          setCountry({ found: false });
+        });
+    }
+  }, [name]);
+
+  return country;
+};
